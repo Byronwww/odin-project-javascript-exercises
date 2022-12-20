426cb1eed2c81ec6077939502f558e9192f3b920
Byron <byronwmit@gmail.com>
2022-12-20 12:37:26 +0000

    Solved Exercise 03

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,15 @@
-const reverseString = function() {
+const reverseString = function(stringToReverse) {
+    let StringToReverseLength = stringToReverse.length;
 
+    let reversedString = '' 
+    //add each letter to an array starting from the last letter and stopping at the first one
+    const reversedStringArray = new Array();
+    for (i=StringToReverseLength; i>=0; i--){
+        let characterToAdd = stringToReverse.charAt(i);
+ 
+        reversedString += characterToAdd;
+    }
+    return reversedString;
 };
 
 // Do not edit below this line
