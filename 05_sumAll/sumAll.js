bb7a2180a379d5294f41006c4bcbf85fb9cfded6
Byron <byronwmit@gmail.com>
2022-12-22 16:07:24 +0000

    Solved Exercise 05

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,36 @@
-const sumAll = function() {
+const sumAll = function(firstNumber, lastNumber) {
 
+var finalSum = 0;
+
+console.log(finalSum);
+
+
+if (typeof firstNumber != "number" || typeof lastNumber != "number"){
+    return "ERROR";
+}
+
+if (firstNumber <0 || lastNumber <0){
+    return "ERROR";
+}
+
+//Swap the numbers around to deal with the first number being larger than the last number
+if (firstNumber > lastNumber){
+    firstNumberSwap = lastNumber;
+    lastNumberSwap = firstNumber 
+}
+else{
+    firstNumberSwap = firstNumber;
+    lastNumberSwap = lastNumber;
+}
+
+    for (i = firstNumberSwap; i<=lastNumberSwap; i++){
+        finalSum = finalSum + i;
+    }
+
+console.log(finalSum);
+return finalSum;
 };
+
 
 // Do not edit below this line
 module.exports = sumAll;
