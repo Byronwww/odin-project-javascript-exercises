d9682e2c0416c6d0337a3ceaacb5c5b5db6969ad
Byron <byronwmit@gmail.com>
2022-12-23 23:23:48 +0000

    Solved Exercise 08

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,27 +1,56 @@
 const add = function(x, y) {
-	let sum = x + y;
-  return sum;
+	let result = x + y;
+  return result;
 };
 
 const subtract = function(x, y) {
-	let sum = x - y;
-  return sum;
+	let result = x - y;
+  return result;
 };
 
-const sum = function() {
-	
+const sum = function(Array) {
+  
+  let result = 0;
+
+  for(i = 0; i < Array.length; i++){
+    result += Array[i];
+  }
+  return result;
+}
+
+const multiply = function(Array) {
+  let result = Array[0];
+
+  for(i = 1; i < Array.length; i++){
+    result = result * Array[i];
+  }
+  return result;  
 };
 
-const multiply = function() {
-
+const power = function(x, y) {
+  let result = x;
+  for(i=2; i<=y; i++){
+    result = result * x;
+  }
+  return result;	
 };
 
-const power = function() {
-	
-};
+// This doesn't feel like a good solution to get the correct answer 
+// for the factorial of 1 but it passes.
+const factorial = function(x) {
+  let result = 1;
 
-const factorial = function() {
-	
+  for(let i = x; i > 0; i--){    
+    y = (i - 1);    
+    result += result * y;
+  }
+
+  if (result === 0){
+    return 1;    
+  }
+  else{
+    return result;
+  }
 };
 
 // Do not edit below this line
