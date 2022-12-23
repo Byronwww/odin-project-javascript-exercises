b8c35c9c625612b43dea3a8c12bb319e51801805
Byron <byronwmit@gmail.com>
2022-12-23 23:40:03 +0000

    First test for Exercise 09 passing, need to get the other tests passing

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,22 @@
-const palindromes = function () {
+const palindromes = function (potentialPalindrome) {
+    
+    let potentialPalindromeLength = potentialPalindrome.length;
 
+    let reversedString = '' 
+    //add each letter to an array starting from the last letter and stopping at the first one
+    const reversedStringArray = new Array();
+    for (i=potentialPalindromeLength; i>=0; i--){
+        let characterToAdd = potentialPalindrome.charAt(i);
+ 
+        reversedString += characterToAdd;
+    }
+
+    if(reversedString == potentialPalindrome){
+        return true;        
+    }
+    else{
+    return false;
+    }
 };
 
 // Do not edit below this line
