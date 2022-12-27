9f0a70a2c8e2ca30234f17a67e15fea450fc83d2
Byron <byronwmit@gmail.com>
2022-12-27 12:43:37 +0000

    Solved Exercise 09

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -2,22 +2,48 @@ const palindromes = function (potentialPalindrome) {
     
     let potentialPalindromeLength = potentialPalindrome.length;
 
-    let reversedString = '' 
-    //add each letter to an array starting from the last letter and stopping at the first one
-    const reversedStringArray = new Array();
-    for (i=potentialPalindromeLength; i>=0; i--){
-        let characterToAdd = potentialPalindrome.charAt(i);
- 
-        reversedString += characterToAdd;
+    let cleanString = '';
+    let reversedString = '';
+
+    //this loop cleans the potentialPalidrome but does not reverse it
+    for (i = 0; i <= potentialPalindromeLength; i++){
+        const nextLetter = potentialPalindrome.charAt(i);
+        
+        if (nextLetter.match(/[a-zA-Z]/)){            
+            console.log("Next Letter: " + nextLetter);
+            cleanString += nextLetter;            
+            console.log(cleanString);
+        }
+    }
+    
+    //This loop reverses the cleanedString and checks if it still matches once reversed  
+    for (i = cleanString.length; i >= 0; i--){
+        const nextLetter = cleanString.charAt(i);
+        
+        if (nextLetter.match(/[a-zA-Z]/)){            
+            console.log(nextLetter);
+            reversedString += nextLetter;            
+
+        }
     }
 
-    if(reversedString == potentialPalindrome){
+    let cleanStringLowerCase = cleanString.toLowerCase();
+    let reversedStringLowerCase = reversedString.toLowerCase();
+
+    console.log("Clean String (lower case): " + cleanStringLowerCase);
+    console.log("Reversed String (lower case): " + reversedStringLowerCase);
+
+
+
+    if(reversedStringLowerCase == cleanStringLowerCase){
         return true;        
     }
     else{
-    return false;
+        return false;
     }
 };
 
+
 // Do not edit below this line
 module.exports = palindromes;
+
