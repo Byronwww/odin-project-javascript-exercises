ddfe59e07083376db3831fb4f9b9697bb8246aa7
Byron <byronwmit@gmail.com>
2022-12-27 13:21:54 +0000

    Solved Exercise 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,4 +1,30 @@
-const fibonacci = function() {
+const fibonacci = function(fibonacciNumber) {
+
+    fibonacciNumber = Number(fibonacciNumber);
+
+    let x = 0;
+    let y = 1;
+    let result = 0;
+
+if (fibonacciNumber < 0){
+    return "OOPS"
+}
+
+if(fibonacciNumber == 1){
+    return 1;
+}
+
+for(i = 0; i < fibonacciNumber-1; i++){    
+    result = x + y;
+    x = y;
+    y = result; 
+    
+    console.log("X: " + x);
+    console.log("Y:" + y);
+    console.log("Result: " + result);
+}
+
+return result;
 
 };
 
