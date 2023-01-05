ecd62b525e53c0a59194334f1ea7924f833b2741
Byron <byronwmit@gmail.com>
2023-01-05 09:00:00 +0000

    Fix Linting issues from Exercise 09

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,46 +1,43 @@
-const palindromes = function (potentialPalindrome) {
-    
-    let potentialPalindromeLength = potentialPalindrome.length;
+const palindromes = function(potentialPalindrome) {
+  const potentialPalindromeLength = potentialPalindrome.length;
 
-    let cleanString = '';
-    let reversedString = '';
+  let cleanString = '';
+  let reversedString = '';
 
-    //this loop cleans the potentialPalidrome but does not reverse it
-    for (i = 0; i <= potentialPalindromeLength; i++){
-        const nextLetter = potentialPalindrome.charAt(i);
-        
-        if (nextLetter.match(/[a-zA-Z]/)){            
-            console.log("Next Letter: " + nextLetter);
-            cleanString += nextLetter;            
-            console.log(cleanString);
-        }
+  // this loop cleans the potentialPalidrome but does not reverse it
+  for (i = 0; i <= potentialPalindromeLength; i++) {
+    const nextLetter = potentialPalindrome.charAt(i);
+
+    if (nextLetter.match(/[a-zA-Z]/)) {
+      console.log('Next Letter: ' + nextLetter);
+      cleanString += nextLetter;
+      console.log(cleanString);
     }
-    
-    //This loop reverses the cleanedString and checks if it still matches once reversed  
-    for (i = cleanString.length; i >= 0; i--){
-        const nextLetter = cleanString.charAt(i);
-        
-        if (nextLetter.match(/[a-zA-Z]/)){            
-            console.log(nextLetter);
-            reversedString += nextLetter;            
+  }
 
-        }
+  // This loop reverses the cleanedString and
+  // checks if it still matches once reversed
+  for (i = cleanString.length; i >= 0; i--) {
+    const nextLetter = cleanString.charAt(i);
+
+    if (nextLetter.match(/[a-zA-Z]/)) {
+      console.log(nextLetter);
+      reversedString += nextLetter;
     }
+  }
 
-    let cleanStringLowerCase = cleanString.toLowerCase();
-    let reversedStringLowerCase = reversedString.toLowerCase();
+  const cleanStringLowerCase = cleanString.toLowerCase();
+  const reversedStringLowerCase = reversedString.toLowerCase();
 
-    console.log("Clean String (lower case): " + cleanStringLowerCase);
-    console.log("Reversed String (lower case): " + reversedStringLowerCase);
+  console.log('Clean String (lower case): ' + cleanStringLowerCase);
+  console.log('Reversed String (lower case): ' + reversedStringLowerCase);
 
 
-
-    if(reversedStringLowerCase == cleanStringLowerCase){
-        return true;        
-    }
-    else{
-        return false;
-    }
+  if (reversedStringLowerCase == cleanStringLowerCase) {
+    return true;
+  } else {
+    return false;
+  }
 };
 
 
