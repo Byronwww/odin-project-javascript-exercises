68ef4017df22162bad0d5aee3eaa589ebdd7e9ed
Byron <byronwmit@gmail.com>
2023-01-05 08:59:18 +0000

    Fix linting issues from exercise 08

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,54 +1,52 @@
 const add = function(x, y) {
-	let result = x + y;
+  const result = x + y;
   return result;
 };
 
 const subtract = function(x, y) {
-	let result = x - y;
+  const result = x - y;
   return result;
 };
 
 const sum = function(Array) {
-  
   let result = 0;
 
-  for(i = 0; i < Array.length; i++){
+  for (i = 0; i < Array.length; i++) {
     result += Array[i];
   }
   return result;
-}
+};
 
 const multiply = function(Array) {
   let result = Array[0];
 
-  for(i = 1; i < Array.length; i++){
+  for (i = 1; i < Array.length; i++) {
     result = result * Array[i];
   }
-  return result;  
+  return result;
 };
 
 const power = function(x, y) {
   let result = x;
-  for(i=2; i<=y; i++){
+  for (i=2; i<=y; i++) {
     result = result * x;
   }
-  return result;	
+  return result;
 };
 
-// This doesn't feel like a good solution to get the correct answer 
+// This doesn't feel like a good solution to get the correct answer
 // for the factorial of 1 but it passes.
 const factorial = function(x) {
   let result = 1;
 
-  for(let i = x; i > 0; i--){    
-    y = (i - 1);    
+  for (let i = x; i > 0; i--) {
+    y = (i - 1);
     result += result * y;
   }
 
-  if (result === 0){
-    return 1;    
-  }
-  else{
+  if (result === 0) {
+    return 1;
+  } else {
     return result;
   }
 };
@@ -60,5 +58,5 @@ module.exports = {
   sum,
   multiply,
   power,
-  factorial
+  factorial,
 };
