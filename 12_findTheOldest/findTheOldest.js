8eced1ca2f20abce1f49fd2559bb6cddfc875754
Byron <byronwmit@gmail.com>
2023-01-05 09:02:50 +0000

    Fix linting issues from exercise 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,44 +1,39 @@
 const findTheOldest = function(input) {
+  const date = new Date();
+  const currentYear = date.getFullYear();
+  console.log('Current Year: ' + currentYear);
 
+  let previousAge = 0;
+  let oldestPerson = '';
+  let currentAge = '';
+  let deathYear = 0;
 
-
-const date = new Date();
-let currentYear = date.getFullYear();
-console.log("Current Year: " + currentYear);
-
-let previousAge = 0;
-let oldestPerson = "";
-let currentAge = "";
-let deathYear = 0;
-
-for (i = 0 ; i < input.length; i++){
+  for (i = 0; i < input.length; i++) {
     console.log(input[i]);
 
-    if (input[i].yearOfDeath > 0){
-        deathYear = input[i].yearOfDeath
-    }
-
-    else{
-        deathYear = currentYear;
+    if (input[i].yearOfDeath > 0) {
+      deathYear = input[i].yearOfDeath;
+    } else {
+      deathYear = currentYear;
     }
 
     currentAge = deathYear - input[i].yearOfBirth;
     currentAge = Number(currentAge);
-    console.log("Current Age: " + currentAge);
-    console.log("Previous Age: " + previousAge);
-        
-        
-    if (currentAge > previousAge){
-        previousAge = currentAge;
-        oldestPerson = input[i];
-    } 
-}
-console.log("This is the oldest person:");  
-console.log(oldestPerson);   
-return oldestPerson;
+    console.log('Current Age: ' + currentAge);
+    console.log('Previous Age: ' + previousAge);
 
-//console.log(input[2].name);
-}
+
+    if (currentAge > previousAge) {
+      previousAge = currentAge;
+      oldestPerson = input[i];
+    }
+  }
+  console.log('This is the oldest person:');
+  console.log(oldestPerson);
+  return oldestPerson;
+
+// console.log(input[2].name);
+};
 
 
 // Do not edit below this line
