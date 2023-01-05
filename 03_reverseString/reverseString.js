42453c1b6840c97ebe3044f531e87303c4cabde7
Byron <byronwmit@gmail.com>
2023-01-05 08:42:57 +0000

    Fix linter issues for Exercise 03

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,19 +1,21 @@
 const reverseString = function(stringToReverse) {
-    let StringToReverseLength = stringToReverse.length;
+  const StringToReverseLength = stringToReverse.length;
 
-    let reversedString = '' 
-    //add each letter to an array starting from the last letter and stopping at the first one
-    const reversedStringArray = new Array();
-    for (i=StringToReverseLength; i>=0; i--){
-        let characterToAdd = stringToReverse.charAt(i);
- 
-        reversedString += characterToAdd;
-    }
-    return reversedString;
+  let reversedString = '';
+  // add each letter to an array starting from the last letter
+  // and stopping at the first one
+  for (i=StringToReverseLength; i>=0; i--) {
+    const characterToAdd = stringToReverse.charAt(i);
+
+    reversedString += characterToAdd;
+  }
+  return reversedString;
 };
 
-//Based on the readme text, I think the exercise expected it to be solved by splitting it into an array 
-//and then using a reverse sort on the array but I prefer this method of just reading the string backwards
+// Based on the readme text, I think the exercise expected
+// it to be solved by splitting it into an array
+// and then using a reverse sort on the array but
+// I prefer this method of just reading the string backwards
 
 // Do not edit below this line
 module.exports = reverseString;
