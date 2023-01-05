8c00a7ddc540a16970e22edb1bf339105be4a0c9
Byron <byronwmit@gmail.com>
2023-01-05 08:52:50 +0000

    Fixed linting issues and learnt how to add JDoc to my function on exercise 04

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,31 +1,31 @@
-//This sends an array (items) plus an array of arguments (itemsToRemove)
-//which need to be filtered out of the items array
+// This sends an array (items) plus an array of arguments (itemsToRemove)
+// which need to be filtered out of the items array
 const removeFromArray = function(items, ...itemsToRemove) {
-
-
-//This is the function that actually does the filtering
-//I need to check a single value being passed in by the filter method 
-//comparing it against each value in the itemsToRemove rest parameter (which is essentially an array)
-
-function filterItems(item){    
-
+/** This is the function that actually does the filtering
+* I need to check a single value being passed in by the filter method
+* comparing it against each value in the itemsToRemove rest parameter
+* (which is essentially an array)
+* @param {array} item - the array to be filtered
+* @return {boolean}
+*/
+  function filterItems(item) {
     for (i = 0; i <= itemsToRemove.length; i++) {
-        if (item === itemsToRemove[i]) return false    
-    }    
-    
-    return true;  
-    }    
+      if (item === itemsToRemove[i]) return false;
+    }
+    return true;
+  }
 
 
-//This returns the filtered array by first calling a function called filterItems 
-//filterItems is where the filtering actually happens
+  // This returns the filtered array by first calling a function called
+  // filterItems
+  // filterItems is where the filtering actually happens
 
-//here's how the filter method works. For each item in your items, 
-//the filter method will call your function (filterItems in this case), 
-//and if your function returns true, it will keep the item inside the array. 
-//But, if your function returns false, it will remove the item from your array.
-//*Thanks to Rachel Banana on Discord for explaining how the filter works ^
-return items.filter(filterItems)
+  // here's how the filter method works. For each item in your items,
+  // the filter method will call your function (filterItems in this case),
+  // and if your function returns true, it will keep the item inside the array.
+  // But, if your function returns false, it will remove the item from  array.
+  // *Thanks to Rachel Banana on Discord for explaining how the filter works ^
+  return items.filter(filterItems);
 };
 
 
