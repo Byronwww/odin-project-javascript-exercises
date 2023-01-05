3919830a5041f59f0fd8cd80434e05eb1ccbe4ff
Byron <byronwmit@gmail.com>
2023-01-05 08:56:47 +0000

    Fix Linting issues on Exercise 07

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,23 +1,19 @@
 const convertToCelsius = function(tempFahrenheit) {
-
-  var convertedTempCelsius;
-  convertedTempCelsius = (tempFahrenheit - 32) * 0.55555555555;  
+  const convertedTempCelsius = (tempFahrenheit - 32) * 0.55555555555;
   console.log(convertedTempCelsius);
-  return Math.round(convertedTempCelsius * 10) / 10 ;
+  return Math.round(convertedTempCelsius * 10) / 10;
 };
 
 
 const convertToFahrenheit = function(tempCelsius) {
-
-  var convertedTempFahrenheit;
-  convertedTempFahrenheit = (tempCelsius / 0.55555555555) + 32;
+  const convertedTempFahrenheit = (tempCelsius / 0.55555555555) + 32;
   console.log(convertedTempFahrenheit);
-  return Math.round(convertedTempFahrenheit * 10) / 10 ;
+  return Math.round(convertedTempFahrenheit * 10) / 10;
 };
 
 
 // Do not edit below this line
 module.exports = {
   convertToCelsius,
-  convertToFahrenheit
-}
+  convertToFahrenheit,
+};
