79229c623df437e6d4c84b96a6c63772fcb9f548
Byron <byronwmit@gmail.com>
2023-01-05 09:03:53 +0000

    Fix linting issues from Exercise 12

--- 13_caesar/caesar.js
+++ 13_caesar/caesar.js
@@ -1,10 +1,10 @@
 const caesar = function(string, shift) {
-    let stringCode = string.charCodeAt(0);
-    console.log(stringCode);
-    let incrementedStringCode = stringCode + shift;
-    console.log(incrementedStringCode);
-    incrementedStringCodeToString = String.fromCharCode(incrementedStringCode);
-    return incrementedStringCodeToString;
+  const stringCode = string.charCodeAt(0);
+  console.log(stringCode);
+  const incrementedStringCode = stringCode + shift;
+  console.log(incrementedStringCode);
+  incrementedStringCodeToString = String.fromCharCode(incrementedStringCode);
+  return incrementedStringCodeToString;
 };
 
 // Do not edit below this line
