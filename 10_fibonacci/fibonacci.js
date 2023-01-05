ee3df1379265452e059796d1d9b79f59b053e5db
Byron <byronwmit@gmail.com>
2023-01-05 09:01:22 +0000

    Fix Linting issues from Exercise 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,31 +1,29 @@
 const fibonacci = function(fibonacciNumber) {
+  fibonacciNumber = Number(fibonacciNumber);
 
-    fibonacciNumber = Number(fibonacciNumber);
+  let x = 0;
+  let y = 1;
+  let result = 0;
 
-    let x = 0;
-    let y = 1;
-    let result = 0;
+  if (fibonacciNumber < 0) {
+    return 'OOPS';
+  }
 
-if (fibonacciNumber < 0){
-    return "OOPS"
-}
-
-if(fibonacciNumber == 1){
+  if (fibonacciNumber == 1) {
     return 1;
-}
+  }
 
-for(i = 0; i < fibonacciNumber-1; i++){    
+  for (i = 0; i < fibonacciNumber-1; i++) {
     result = x + y;
     x = y;
-    y = result; 
-    
-    console.log("X: " + x);
-    console.log("Y:" + y);
-    console.log("Result: " + result);
-}
+    y = result;
 
-return result;
+    console.log('X: ' + x);
+    console.log('Y:' + y);
+    console.log('Result: ' + result);
+  }
 
+  return result;
 };
 
 // Do not edit below this line
